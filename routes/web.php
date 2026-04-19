b13224259087666dfba44616b828f086a3946bcb
Théo Vandormael <theo.vandormael@proton.me>
2026-04-19 18:00:07 +0200

    wip: color update and news article page creation

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Home')->name('home');
 Route::inertia('/agenda', 'Agenda')->name('agenda');
 Route::inertia('/archives', 'Archives')->name('archives');
 Route::inertia('/news', 'News')->name('news');
+Route::get('/news/{slug}', function (string $slug) {
+	return Inertia::render('NewsShow', [
+		'slug' => $slug,
+	]);
+})->name('news.show');
