2d11561dacccfcead943b935b2e1a981cd6de69a
Théo Vandormael <theo.vandormael@proton.me>
2026-04-28 23:35:40 +0200

    add a show page for events

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,19 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Home')->name('home');
-Route::inertia('/agenda', 'Agenda')->name('agenda');
+
+Route::inertia('/agenda', 'Events/Index')->name('agenda');
+Route::get('/events/{slug}', function (string $slug) {
+    return Inertia::render('Events/Show', [
+        'slug' => $slug,
+    ]);
+})->name('events.show');
+
 Route::inertia('/archives', 'Archives')->name('archives');
-Route::inertia('/news', 'News')->name('news');
+
+Route::inertia('/news', 'News/Index')->name('news');
 Route::get('/news/{slug}', function (string $slug) {
-	return Inertia::render('NewsShow', [
-		'slug' => $slug,
-	]);
+    return Inertia::render('News/Show', [
+        'slug' => $slug,
+    ]);
 })->name('news.show');
