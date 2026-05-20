6063d236f92f49844754b364c6593c384341a109
Théo Vandormael <73583554+theo-vdml@users.noreply.github.com>
2026-05-20 22:23:51 +0200

    Dev (#14)
    * fix: seo title not working
    
    * fix: use supervisord for queue worker
    
    * fix: reduce Caddy log level

--- start-container.sh
+++ start-container.sh
@@ -17,11 +17,11 @@ if [ "$IS_LARAVEL" = "true" ]; then
   echo "Optimizing Filament admin panel ..."
   php artisan filament:optimize
 
-  echo "Starting Laravel queue worker ..."
-  php artisan queue:work \
-    --sleep=3 \
-    --tries=3 \
-    --timeout=60 &
+#   echo "Starting Laravel queue worker ..."
+#   php artisan queue:work \
+#     --sleep=3 \
+#     --tries=3 \
+#     --timeout=60 &
 
 #   echo "Starting Laravel server ..."
 fi
